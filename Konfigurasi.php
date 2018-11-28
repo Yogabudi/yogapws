f55f1c8b725b94c9ecd759e7e5e3dc318ffb975a
Yoga Budi Yulianto <yogabudiyulianto@gmail.com>
2018-11-28 08:16:50 +0700

    penambahan variabel lokasi web service

---
+++ Konfigurasi.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * Author : Yoga Budi Yulianto
+ * 
+ * Class InfoDB menyimpan informasi database
+ * Ubah value sesuai informasi database anda
+ * 
+ */
+
+class Konfigurasi {
+  // lokasi web service pada server
+  // contoh : http://localhost/webservice (tanpa diakhiri "/" )
+  public static $LOKASIWS = "<masukkan lokasi web"; 
+
+  // nama database
+  // contoh : db_buku
+  public static $NAMA_DB = "<masukkan nama db>";
+
+  // url hosting
+  // contoh : localhost
+  public static $HOST = "<masukkan host>";    
+  
+  // username database
+  // contoh : root
+  public static $USER = "<masukkan user>";
+
+  // password database
+  public static $PASSWORD = "<masukkan password>";
+}
+
+?>
