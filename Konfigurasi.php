9c800c8c7366ec15b619f1bc11e8f0c36a8c9ff4
Yoga Budi Yulianto <yogabudiyulianto@gmail.com>
2019-02-22 10:20:10 +0700

    penambahan file release

--- Konfigurasi.php
+++ Konfigurasi.php
@@ -11,19 +11,19 @@
 class Konfigurasi {
   // lokasi web service pada server
   // contoh : http://localhost/webservice (tanpa diakhiri "/" )
-  public static $LOKASIWS = ""; 
+  public static $LOKASIWS = "http://localhost/yogapws"; 
 
   // nama database
   // contoh : db_buku
-  public static $NAMA_DB = "";
+  public static $NAMA_DB = "db_buku";
 
   // url hosting
   // contoh : localhost
-  public static $HOST = "";    
+  public static $HOST = "localhost";    
   
   // username database
   // contoh : root
-  public static $USER = "";
+  public static $USER = "root";
 
   // password database
   public static $PASSWORD = "";
